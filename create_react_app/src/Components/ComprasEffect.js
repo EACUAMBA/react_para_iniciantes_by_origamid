e216454af05564f159df0aa9001350301a492038
Edilson Alexandre Cuamba <edilson.cuamba@exi.co.mz>
2022-03-19 14:09:44 +0200

    Hooks useEffect

--- create_react_app/src/Components/ComprasEffect.js
+++ create_react_app/src/Components/ComprasEffect.js
@@ -5,10 +5,21 @@ function ComprasEffect(props) {
 
     const [getTitle, setTitle] = useState("React");
     const [count, setCount] = useState(1);
+    const [moedas, setMoedas] = useState([])
 
     useEffect(()=>{
         document.title = getTitle;
     }, [getTitle]);
+
+    useEffect(()=>{
+        fetch("https://currencyapi.net/api/v1/currencies?key=7JhHIydpksuvlRBzR7KsFR1FTvQ4Oib8S3BB&output=JSON")
+            .then((response)=>{
+                console.log(response)
+                return response.json();
+            }).then((data)=>{
+               setMoedas(Object.entries(data.currencies))
+        })
+    }, [])
 
     function handleChange(event){
         console.log(event.target.value)
@@ -23,6 +34,26 @@ function ComprasEffect(props) {
         <div>
             <input type={'text'} name={'title'} onChange={handleChange}/>
             <input type={'number'} name={'count'} onChange={handleChangeCount}/>
+            <div>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Moeda</th>
+                        <th>País</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {moedas.map((m)=>{
+                        return (
+                            <tr>
+                                <td>{m[0]}</td>
+                                <td>{m[1]}</td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
